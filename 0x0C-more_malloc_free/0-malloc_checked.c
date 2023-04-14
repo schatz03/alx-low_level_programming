2e2e7afc989dd1e60f72c8ea6ff9e920302cac5f
Schatz03 <nourisoukaina11@gmail.com>
2023-04-14 08:51:48 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * malloc_checked - cause normal process termination with k  status value of 98
+ *
+ * @b: allocated memory
+ * @schaz03
+ * Return: k  pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *j;
+
+	j = malloc(b);
+	if (j == NULL)
+		exit(98);
+	return (j);
+
+}
