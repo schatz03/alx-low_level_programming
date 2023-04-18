d550c6ab8fc696ca52facbfd657381e78274f1fd
Schatz03 <nourisoukaina11@gmail.com>
2023-04-18 05:53:10 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -109,7 +109,7 @@ void init(char *str, int l)
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
-	char *a;
+	char *b;
 	char *t;
 	char e[] = "Error\n";
 
@@ -124,25 +124,25 @@ int main(int argc, char *argv[])
 	for (l2 = 0; argv[2][l2]; l2++)
 		;
 	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
+	b = malloc(ln * sizeof(char));
+	if (b == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
 			_putchar(e[ti]);
 		exit(98);
 	}
-	init(a, ln - 1);
+	init(b, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		t = mul(argv[2][ti], argv[1], l1 - 1, b, (ln - 2) - i);
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
 				_putchar(e[ti]);
-			free(a);
+			free(b);
 			exit(98);
 		}
 	}
-	_print(a, ln - 1);
+	_print(b, ln - 1);
 	return (0);
 }
