6843335253846ff49952899bf3b27e524d1f76d6
Schatz03 <nourisoukaina11@gmail.com>
2023-04-28 15:41:38 -0700

    rite a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+section           .data
+        
+
+    message db "Hello, Holberton", 0xa,0
+
+section           .text
+         extern   printf
+         global    main
+main:
+    mov         edi, messgae
+    mov         eax, 0
+    call        printf
+    syscall
