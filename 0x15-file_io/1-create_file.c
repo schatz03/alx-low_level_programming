44aaa395e7ab0b4a1dd0cf0652229719259392c9
Schatz03 <nourisoukaina11@gmail.com>
2023-05-09 06:02:46 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fo, rw, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+	if (text_content != NULL)
 	fo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fo == -1)
 		return (-1);
