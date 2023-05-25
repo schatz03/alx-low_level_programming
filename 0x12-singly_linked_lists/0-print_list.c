e015abd29d0362a0bb7250e3026acacb8f102c24
Schatz03 <nourisoukaina11@gmail.com>
2023-05-25 11:18:36 -0700

    function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,6 +6,23 @@
  *
  * Return: size of lists
  */
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
 size_t print_list(const list_t *h)
 {
 	int i = 0;
