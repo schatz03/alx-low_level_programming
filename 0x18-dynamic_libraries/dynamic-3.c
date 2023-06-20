50be2340e0cab4d09907757e64445c522aa05586
Schatz03 <nourisoukaina11@gmail.com>
2023-06-20 03:09:03 -0700

    lib

---
+++ 0x18-dynamic_libraries/dynamic-3.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area
+ * @dest: The destination memory area
+ * @src: The source memory area
+ * @n: The number of bytes to be copied
+ *
+ * Return: A pointer to the destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	/* Implementation goes here */
+}
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string
+ * @c: The character to be located
+ *
+ * Return: A pointer to the first occurrence of the character in the string,
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	/* Implementation goes here */
+}
+
+/**
+ * _strspn - Calculates the length of the initial segment of a string
+ *            consisting of only characters from another string
+ * @s: The string
+ * @accept: The string containing the characters to match
+ *
+ * Return: The length of the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	/* Implementation goes here */
+}
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: The string to be searched
+ * @accept: The bytes to be located
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	/* Implementation goes here */
+}
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: The string to be searched
+ * @needle: The substring to be found
+ *
+ * Return: A pointer to the first occurrence of the substring in the string,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	/* Implementation goes here */
+}
