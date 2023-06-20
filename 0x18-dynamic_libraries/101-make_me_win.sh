fc96f3ddcaa072e7f7dddec8503da5978ad9a8dd
Schatz03 <nourisoukaina11@gmail.com>
2023-06-20 10:39:27 -0700

    libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/will.so https://github.com/schatz03/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -O /tmp/will.so https://github.com/schatz03/alx-low_level_programming/tree/master/0x18-dynamic_libraries/will.so
+export LD_PRELOAD=/tmp/will.so
